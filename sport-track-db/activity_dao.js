dbbbd589a6dbb0b59dbe27abd920af0c6e3c7d5a
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-10-14 17:13:11 +0200

    création de toutes les méthodes d'activity

--- sport-track-db/activity_dao.js
+++ sport-track-db/activity_dao.js
@@ -1,29 +1,27 @@
 var db = require('./sqlite_connection');
 
 activityDAO = function() {
-    this.insert = function(values) {
+    this.insert = function(activity, callback) {
         let query = "insert into Activities(description,date,startTime,duration,distance,cardiacFreqMin,cardiacFreqAvg, cardiacFreqMax,idUser) values ($desc,$date,$st,$du,$dis,$cFreqMin,$cFreqAvg,$cFreqMax,$idUser)";
-        db.run(query, values, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
+        values = [activity.getDesc(), activity.getDate(), activity.getStartTime(), activity.getDuration(), activity.getDistance(), activity.getCardiacFreqMin(), activity.getCardiacFreqAvg(), activity.getCardiacFreqMax(), activity.getIdUser()];
+        db.run(query, values, callback);
     };
 
-    this.selectAcitvity = function(user, callback) {
-        let query = "SELECT * FROM Activity WHERE eMail = $eMail AND password = $password";
-        values = [user.getMail(), user.getPassword()];
+    this.selectActivity = function(activity, callback) {
+        let query = "select * from Activities where idUser = $idUser and description = $desc and date = $date and startTime = $st and duration = $du and distance = $dis and cardiacFreqMin = $cFreqMin and cardiacFreqAvg = $cFreqAvg and cardiacFreqMax = $cFreqMax ";
+        values = [activity.getIdUser(), activity.getDesc(), activity.getDate(), activity.getStartTime(), activity.getDuration(), activity.getDistance(), activity.getCardiacFreqMin(), activity.getCardiacFreqAvg(), activity.getCardiacFreqMax()];
         db.get(query, values, callback);
     }
 
-    this.update = function(values) {
-        let query = "update Activities set description = $desc , date = $date,startTime = $st,duration = $du ,distance = $dis,cardiacFreqMin = $cFreqMin, cardiacFreqAvg = $cFreqAvg, cardiacFreqMax = $cFreqMax, idUser = $idUser WHERE idAct = $id";
+    this.update = function(activity, key, callback) {
+        let query = "update Activities set description = $desc , date = $date,startTime = $st,duration = $du ,distance = $dis,cardiacFreqMin = $cFreqMin, cardiacFreqAvg = $cFreqAvg, cardiacFreqMax = $cFreqMax, idUser = $idUser WHERE idAct =" + key;
+        values = [activity.getDesc(), activity.getDate(), activity.getStartTime(), activity.getDuration(), activity.getDistance(), activity.getCardiacFreqMin(), activity.getCardiacFreqAvg(), activity.getCardiacFreqMax(), activity.getIdUser()];
         db.run(query, values);
 
     };
-    this.delete = function(key) {
+    this.delete = function(values, callback) {
         let query = "delete from Activities where idAct = ?";
-        db.run(query, key);
+        db.run(query, values, callback);
     };
     this.findAll = function(callback) {
         let query = "SELECT * FROM Activities";
@@ -32,15 +30,11 @@ activityDAO = function() {
 
     this.findByKey = function(values, callback) {
         let query = "SELECT * FROM Activities WHERE idAct = ?";
-        db.run(query, values, callback);
+        db.get(query, values, callback);
     };
-    this.deleteAll = function() {
+    this.deleteAll = function(callback) {
         let query = "delete from Activities";
-        db.run(query, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
+        db.run(query, callback);
     };
 };
 var dao = new activityDAO();
--- sport-track-db/sport-track-db-test.js
+++ sport-track-db/sport-track-db-test.js
@@ -6,62 +6,183 @@ var db = require('./sport-track-db').db_connection;
 
 
 
+var user = new User();
+var user2 = new User();
+var activity = new Activity();
 
 user_dao.deleteAll(function(err) {
     if (err) {
         console.log(err);
     }
+
+    testInsertUser();
 });
 
 console.log("All users deleted");
-
-
 console.log("TEST : UserDAO");
 
-var user = new User();
-var user2 = new User();
-user.init("Doe", "John", "1990-01-01", "M", 180, 80, "john.eMail@gmail.com", "123456");
-user2.init("Besily", "Michaël", "2003-10-01", "M", 180, 80, "Michael.besily@gmail.com", "123456");
 
-user_dao.insert(user, (err) => {
-    if (err) {
-        console.log(err);
-    }
-});
+function testInsertUser() {
+    user.init("Doe", "John", "1990-01-01", "M", 180, 80, "john.eMail@gmail.com", "123456");
+    user2.init("Besily", "Michaël", "2003-10-01", "M", 180, 80, "Michael.besily@gmail.com", "123456");
+    user_dao.insert(user, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        testSelectUser();
+    });
+}
+
+function testSelectUser() {
+    user_dao.selectUser(user, (err, row) => {
+        if (err) {
+            console.log(err);
+        }
+        var user = row
+        var idUser = user.idUser
+        console.log("User inserted with id : " + idUser);
+        testfindByKey(idUser);
+    });
+}
 
 
-user_dao.selectUser(user, (err, row) => {
-    if (err) {
-        console.log(err);
-    }
-    var idUser = row.idUser
-    console.log("User inserted with id : " + idUser);
+function testfindByKey(idUser) {
     console.log("Test : findByKey ")
     user_dao.findByKey(idUser, (err, row) => {
         if (err) {
             console.log(err);
         }
         console.log(row);
+        testUpdateUser(idUser);
     })
+}
+
+function testUpdateUser(idUser) {
     console.log("Test : update ")
     user_dao.update(user2, idUser, (err) => {
         if (err) {
             console.log(err);
         }
         console.log("User updated");
-        console.log("Test : findAll ")
-        user_dao.findAll((err, rows) => {
+    })
+    testDeleteUser(idUser);
+};
+
+
+function testDeleteUser(idUser) {
+    console.log("Test : delete ")
+    user_dao.delete(idUser, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("User deleted");
+        testDeleteAllActivities();
+    })
+}
+
+
+
+function testDeleteAllActivities() {
+    console.log("TEST : ActivityDAO");
+    activity_DAO.deleteAll((err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("All activities deleted");
+        testInsertActivity();
+    })
+}
+
+function testInsertActivity() {
+    console.log("Test : insert Activity")
+    user_dao.insert(user, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        selectUser(user, (err, id) => {
             if (err) {
                 console.log(err);
             }
-            console.log(rows);
-            user_dao.delete(idUser, (err) => {
+            var idUser = id
+            console.log("User inserted with id : " + idUser);
+            activity.init("Pétanque", "18/08/2022", "16:00:00", "00:10:00", 10, 75, 110, 150, idUser);
+            activity_DAO.insert(activity, (err) => {
                 if (err) {
                     console.log(err);
                 }
-                console.log("User deleted");
+                console.log("Activity inserted");
+                selectActivity(activity, (err, idAct) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    var idAct = idAct
+                    console.log("Activity inserted with id : " + idAct);
+                    testFindByKeyActivities(idAct);
+                })
             })
+
         })
     })
+}
 
-});
+
+function selectUser(user, callback) {
+    user_dao.selectUser(user, (err, row) => {
+        if (err) {
+            console.log(err);
+        }
+        var user = row
+        callback(err, user.idUser);
+    });
+}
+
+function selectActivity(activity, callback) {
+    activity_DAO.selectActivity(activity, (err, row) => {
+        if (err) {
+            console.log(err);
+        }
+        var activity = row
+        callback(err, activity.idAct);
+    });
+}
+
+
+function testFindByKeyActivities(idAct) {
+    console.log("Test : findByKey Activity")
+    activity_DAO.findByKey(idAct, (err, row) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(row);
+        testUpdateActivity(idAct);
+    })
+}
+
+function testUpdateActivity(idAct) {
+    console.log("Test : update Activity")
+    selectUser(user, (err, id) => {
+        if (err) {
+            console.log(err);
+        }
+        var idUser = id
+        console.log("User inserted with id : " + idUser);
+        activity.init("FootBall", "18/08/2022", "18:00:00", "00:05:00", 10, 75, 110, 150, idUser);
+        activity_DAO.update(activity, idAct, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Activity updated");
+        })
+    })
+    testDeleteActivity(idAct);
+}
+
+function testDeleteActivity(idAct) {
+    console.log("Test : delete Activity")
+    activity_DAO.delete(idAct, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Activity deleted");
+    })
+}

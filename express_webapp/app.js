a7f5b20e11a0d2a7fe5a15709ceedbc73da13138
Michaël <michael.besily@gmail.com>
2022-10-19 20:54:38 +0200

    création des différentes vues

--- express_webapp/app.js
+++ express_webapp/app.js
@@ -9,9 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// add view
-var users = require('./routes/users');
-app.use('/users', users);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/connect', connectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express_webapp/routes/users.js
+++ express_webapp/routes/users.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var user_dao = require('sport-track-db').user_dao;
+var User = require('sport-track-db').user;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    user_dao.findAll(function(err, rows) {
-        if (err != null) {
-            console.log("ERROR= " + err);
+
+/* Add a new user */
+router.post('/', function(req, res, next) {
+    var user = new User();
+    user.init(req.body.surname, req.body.name, req.body.date, req.body.gender, req.body.size, req.body.weight, req.body.email, req.body.password);
+    user_dao.addUser(user, function(err, user) {
+        if (err) {
+            res.status(500).send(err);
         } else {
-            res.render('users', { data: rows });
+            res.status(201).send(user);
         }
     });
 });

91f68a5d2d4590f10a979c565de3fb68bdc2e0e6
Michaël <michael.besily@gmail.com>
2022-10-19 18:43:42 +0200

    Finalisation de la première vue de test users

--- express_webapp/routes/users.js
+++ express_webapp/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var user_dao = require('sport-track-db').user_dao;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    user_dao.findAll(function(err, rows) {
+        if (err != null) {
+            console.log("ERROR= " + err);
+        } else {
+            res.render('users', { data: rows });
+        }
+    });
 });
-
 module.exports = router;

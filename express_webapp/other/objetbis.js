41fbad508ff86171aca02c1a6f953b97bef1be7d
Michaël <michael.besily@gmail.com>
2022-10-20 22:40:49 +0200

    upload fonctionnelle

--- express_webapp/other/objetbis.js
+++ express_webapp/other/objetbis.js
@@ -1,57 +1,57 @@
-class CalculDistance {
+module.exports =
+    class CalculDistance {
+        constructor() {}
+        __construct() {}
+            /**
+             * Retourne la distance en mètres entre 2 points GPS exprimés en degrés.
+             * @param float $lat1 Latitude du premier point GPS
+             * @param float $long1 Longitude du premier point GPS
+             * @param float $lat2 Latitude du second point GPS
+             * @param float $long2 Longitude du second point GPS
+             * @return float La distance entre les deux points GPS
+             */
 
-    /**
-     * Retourne la distance en mètres entre 2 points GPS exprimés en degrés.
-     * @param float $lat1 Latitude du premier point GPS
-     * @param float $long1 Longitude du premier point GPS
-     * @param float $lat2 Latitude du second point GPS
-     * @param float $long2 Longitude du second point GPS
-     * @return float La distance entre les deux points GPS
-     */
-
-    R = 6378.137;
-
-
-    degreVersRad(deg) {
-        return Math.PI * (deg) / 180;
-    }
+        R = 6378.137;
 
 
-    calculDistance2PointsGPS(lat1, long1, lat2, long2) {
-        lat1 = this.degreVersRad(lat1);
-        lat2 = this.degreVersRad(lat2);
-        long1 = this.degreVersRad(long1);
-        long2 = this.degreVersRad(long2);
-        return (this.R * Math.acos(Math.sin(lat2) * Math.sin(lat1) + Math.cos(lat2) * Math.cos(lat1) * Math.cos(long2 - long1)));
-    }
-
-
-    /**
-     * Retourne la distance en mètres du parcours passé en paramètres. Le parcours est
-     * défini par un tableau ordonné de points GPS.
-     * @param Array $parcours Le tableau contenant les points GPS
-     * @return float La distance du parcours
-     */
-
-    calculDistanceTrajet(parcours) {
-        var distTotale = 0;
-        for (let i = 0; i < parcours.data.length - 1; i++) {
-            distTotale += this.calculDistance2PointsGPS(
-                (parcours.data[i]["latitude"]),
-                (parcours.data[i]["longitude"]),
-
-                (parcours.data[i + 1]["latitude"]),
-                parcours.data[i + 1]["longitude"]);
+        degreVersRad(deg) {
+            return Math.PI * (deg) / 180;
         }
-        return distTotale * 1000;
+
+
+        calculDistance2PointsGPS(lat1, long1, lat2, long2) {
+            lat1 = this.degreVersRad(lat1);
+            lat2 = this.degreVersRad(lat2);
+            long1 = this.degreVersRad(long1);
+            long2 = this.degreVersRad(long2);
+            return (this.R * Math.acos(Math.sin(lat2) * Math.sin(lat1) + Math.cos(lat2) * Math.cos(lat1) * Math.cos(long2 - long1)));
+        }
+
+
+        /**
+         * Retourne la distance en mètres du parcours passé en paramètres. Le parcours est
+         * défini par un tableau ordonné de points GPS.
+         * @param Array $parcours Le tableau contenant les points GPS
+         * @return float La distance du parcours
+         */
+
+        calculDistanceTrajet(parcours) {
+            var distTotale = 0;
+            for (let i = 0; i < parcours.data.length - 1; i++) {
+                distTotale += this.calculDistance2PointsGPS(
+                    (parcours.data[i]["latitude"]),
+                    (parcours.data[i]["longitude"]),
+
+                    (parcours.data[i + 1]["latitude"]),
+                    parcours.data[i + 1]["longitude"]);
+            }
+            return distTotale * 1000;
+        }
+
+
+
     }
 
-
-
-}
-
-
-var classCalc = new CalculDistance();
 
 trajet = {
     "activity": {
@@ -67,8 +67,3 @@ trajet = {
         { "time": "13:00:25", "cardio_frequency": 103, "latitude": 47.648510, "longitude": -2.780145, "altitude": 16 }
     ]
 }
-
-
-
-
-console.log(classCalc.calculDistanceTrajet(trajet))
--- express_webapp/routes/upload.js
+++ express_webapp/routes/upload.js
@@ -1,13 +1,11 @@
-var express = require("express");
-var router = express.Router();
-//model
 const Activity = require('../sport-track-db/Activity');
 const Data = require('../sport-track-db/Data');
-//DAO
 var activity_dao = require('../sport-track-db/activity_dao');
 var data_dao = require('../sport-track-db/activity_entry_dao');
-//calculs
-const calc = require('../other/objet.js');
+var CalculDistance = require('../other/objetbis');
+var express = require("express");
+const { activityEntryDAO } = require('../sport-track-db/sport-track-db');
+var router = express.Router();
 
 
 router.get("/", (req, res) => {
@@ -16,101 +14,82 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res, next) => {
     try {
-        //récupérer les infos du fichier
         json_data = JSON.parse(req.files.activites.data);
 
-        //rentrer les données du json dans la bdd
         dataArray = new Array();
-
-        //parcourir le json
-        for (const value in json_data) {
-            if (value == "data") {
-                //récupérer les données de chaque data de l'activité et les sauvegarder
-                json_data[value].map((data) => {
+        for (const data in json_data) {
+            if (data == "data") {
+                json_data[data].map((data) => {
                     time = data.time;
                     cFreq = data.cardio_frequency;
                     latitude = data.latitude;
                     longitude = data.longitude;
                     altitude = data.altitude;
-
-                    //sauvegarde dans un tableau annexe
-                    dataArray.push(time, cFreq, latitude, longitude, altitude);
+                    dataArray.push(time, cFreq, longitude, latitude, altitude);
                 });
-            } else if (value == "activity") {
-                //récupérer la date et la description de l'activité
-                date = json_data[value].date;
-                desc = json_data[value].description;
+            } else if (data == "activity") {
+                date = json_data[data].date;
+                desc = json_data[data].description;
             } else {
                 res.render("error", { message: "Erreur lors de l'importation du fichier", error: { status: "500", stack: "Erreur lors du parcours du fichier" } });
             }
         }
 
-        //récupérer l'heure de début et de fin de l'activité et calculer la différence en convertissant en date
-        var st = new Date(Date.parse("Janvier 01, 1999 " + dataArray[0]));
-        var et = new Date(Date.parse("Janvier 01, 1999 " + dataArray[dataArray.length - 5]));
-        //calculer la différence entre heure de déprt et d'arrivée et la convertir en string
-        var diff = msToHMS(et.getTime() - st.getTime());
 
-        //calculer la distance du parcours de l'activité 
-        classCalc = new calc();
-        let parcours = new Object();
-        parcours.nom = "activity";
+        var startTime = dataArray[0]
+        var duration = new Date(Date.parse("Janvier 01, 1999 " + dataArray[dataArray.length - 5])) - new Date(Date.parse("Janvier 01, 1999 " + startTime));
 
-        //tableau d'objets contenant les coordonnées de chaque point du parcours
+        var classCalc = new CalculDistance();
         data = new Array();
-
         for (let i = 2; i < dataArray.length; i += 5) {
-            data.push({ 'lat': dataArray[i], 'lon': dataArray[i + 1] });
+            data.push({ 'latitude': dataArray[i], 'longitude': dataArray[i + 1] });
         }
 
-        parcours.data = data;
-        var distTotale = classCalc.calculDistanceTrajet(parcours);
+        var distance = classCalc.calculDistanceTrajet({ 'data': data });
 
-        //récupérer les différentes fréquences cardiaques
-        var minVal = 0;
-        var maxVal = 2147483647;
-        var avgVal = 0;
-        var count = 0;
-        var add = 0;
 
-        //parcourir toutes les fréquences cardiaques
-        for (let i = 1; i < dataArray.length; i += 5) {
-            if (dataArray[i] < maxVal) {
-                maxVal = dataArray[i];
+
+        var minCardio = dataArray[1];
+        var avgCardio = dataArray[1];
+        var maxCardio = dataArray[1];
+        for (let i = 6; i < dataArray.length; i += 5) {
+            if (dataArray[i] < minCardio) {
+                minCardio = dataArray[i];
             }
-            if (dataArray[i] > minVal) {
-                minVal = dataArray[i];
+            if (dataArray[i] > maxCardio) {
+                maxCardio = dataArray[i];
             }
-            add = add + dataArray[i];
-            count++;
+            avgCardio += dataArray[i];
         }
+        avgCardio = avgCardio / (dataArray.length / 5);
 
-        tmp = minVal;
-        minVal = maxVal; //fréquence cardiaque minimum
-        maxVal = tmp; //fréquence cardiaque maximum
-        avgVal = add / count; //fréquence cardiaque moyenne
 
-        //init activity
         if (req.session.idUser == null) {
             res.redirect("connect");
         } else {
-            var act = new Activity();
-            act.init(1, desc, date, dataArray[0], diff, distTotale, minVal, avgVal, maxVal, req.session.idUser);
+            var activity = new Activity();
+            activity.init(desc, date, startTime, duration, distance, minCardio, avgCardio, maxCardio, req.session.idUser);
 
-            //insert activityDAO
-            activity_dao.insert(act, act.toTab()).then((result) => {
-                //init data
-                for (let i = 0; i < dataArray.length; i += 5) {
-                    //init data
-                    data = new Data();
-                    data.init(123, dataArray[i], dataArray[i + 1], dataArray[i + 3], dataArray[i + 2], dataArray[i + 4], act.getIdAct());
-
-                    //insert activityEntryDAO
-                    data_dao.insert(data, data.toTab());
+            activity_dao.insert(activity, (err) => {
+                if (err) {
+                    console.log(err);
                 }
+                activity_dao.selectActivity(activity, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    var activity = row;
+                    var idAct = activity.idAct;
+                    var data = new Data();
+                    data.init(startTime, dataArray[2], dataArray[3], dataArray[4], idAct);
+                    data_dao.insert(data, (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        res.render("upload_valid", { title: "Importer un fichier", message: "Fichier importé avec succès" });
+                    });
+                });
             });
-
-            res.render('upload_valid', { title: 'Fichier importé' });
         }
     } catch (error) {
         console.error(error);
@@ -120,24 +99,5 @@ router.post("/", (req, res, next) => {
         });
     }
 });
-
-/**
- * Convertit une durée en millisecondes en une durée en heures, minutes et secondes
- * @param {Number} ms Durée en millisecondes
- * @returns {String} résultat de la conversion
- */
-function msToHMS(ms) {
-    // Convertir en secondes
-    let seconds = ms / 1000;
-    // Extraire les heures
-    const hours = parseInt(seconds / 3600);
-    seconds = seconds % 3600;
-    // Extraire les minutes
-    const minutes = parseInt(seconds / 60);
-    // Nombre de secondes restantes
-    seconds = seconds % 60;
-
-    return hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
-}
 
 module.exports = router;
